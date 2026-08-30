ec796fdacfcb9a61c3ec2b9fc5d224caa3c06ae7
Your Name <you@example.com>
2026-08-30 18:29:15 +0100

    feat: Added cors to the server api

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,5 +1,6 @@
 
 from flask import Flask
+from flask_cors import CORS
 
 from .blueprints.health import health
 
@@ -7,6 +8,12 @@ from .blueprints.health import health
 def create_app():
     app = Flask(__name__)
 
+    CORS(app, resources={
+        r"/api/*": {
+            "origins": "*"
+        }
+    })
+    
     app.register_blueprint(health, url_prefix="/api")
 
     return app
