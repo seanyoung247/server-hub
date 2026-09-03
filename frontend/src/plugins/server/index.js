183fb7c13ee996d26342a7828336a50e5e91cc24
Your Name <you@example.com>
2026-09-03 20:15:25 +0100

    feat: Added initial server card

--- frontend/src/plugins/server/index.js
+++ frontend/src/plugins/server/index.js
@@ -1,11 +1,23 @@
 
-import { Enso, html } from "ensojs";
+import { Enso, html, prop } from "ensojs";
+
+import "../../components/card.enso";
 
 
-export default Enso.component('enso-server-display', {
+export default Enso.component('enso-server-card', {
+    watched: {
+        server: prop(null)
+    },
 
     template: html`
-        
+        <enso-card>
+            <h2>
+                <span>Server</span>
+                <span class="{{ @:server ? 'online' : 'offline' }}">
+                    {{ @:server ? 'online' : 'offline' }}
+                </span>
+            </h2>
+        </enso-card>
     `,
 });
 
