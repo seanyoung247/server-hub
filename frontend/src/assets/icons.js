8fbb43e19f42193722fb56ffa14deb23baf30e80
Sean Young <seanyoung247@gmail.com>
2026-09-04 14:14:43 +0100

    refactor: Minor code tweaks

--- frontend/src/assets/icons.js
+++ frontend/src/assets/icons.js
@@ -1,5 +1,7 @@
 
-import audiobooks from "./audiobook.svg?raw"
+import {Enso, html} from "ensojs";
+
+import audiobooks from "./audiobook.svg?raw";
 import jellyfin from "./jellyfin.svg?raw";
 import nextcloud from "./nextcloud.svg?raw";
 import pihole from "./pi-hole.svg?raw";
@@ -7,6 +9,16 @@ import router from "./router.svg?raw";
 import server from "./server.svg?raw";
 import reload from "./reload.svg?raw";
 
-export default {
+
+const rawIcons = {
     audiobooks, jellyfin, nextcloud, pihole, router, server, reload
+};
+export default rawIcons;
+
+export const Icons = {};
+for (const icon in rawIcons) {
+    Icons[icon] = Enso.component(`enso-icon-${icon}`, {
+        settings: { useShadow: false },
+        template: html`${rawIcons[icon]}`
+    });
 }
