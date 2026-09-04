56822410c5beb9dd11428f666fce6a69cb6857bb
Sean Young <seanyoung247@gmail.com>
2026-09-04 03:30:01 +0100

    feat: Added Refresh icon

--- frontend/src/assets/icons.js
+++ frontend/src/assets/icons.js
@@ -5,7 +5,8 @@ import nextcloud from "./nextcloud.svg?raw";
 import pihole from "./pi-hole.svg?raw";
 import router from "./router.svg?raw";
 import server from "./server.svg?raw";
+import reload from "./reload.svg?raw";
 
 export default {
-    audiobooks, jellyfin, nextcloud, pihole, router, server
+    audiobooks, jellyfin, nextcloud, pihole, router, server, reload
 }
